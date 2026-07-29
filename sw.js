5c74eaad59af133c1b8fccd7d4a196c98fc4ac55
tad551216-rgb <tad551216@gmail.com>
2026-07-29 16:25:02 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'hiyari-techo-v18';
-const ASSETS = ['./','./index.html','./manifest.json','./qrcode.js','./jsQR.js','./zxing.min.js','./icon-192.png','./icon-512.png','./icon-180.png'];
+const CACHE = 'hiyari-techo-v19';
+const ASSETS = ['./','./index.html','./manifest.json','./qrcode.js','./fg-barcode-scanner.min.js','./icon-192.png','./icon-512.png','./icon-180.png'];
 
 /* 入れておく。1つ失敗しても、ほかは入れる */
 self.addEventListener('install', e=>{
